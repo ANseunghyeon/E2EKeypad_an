f2310ebf0fd8c51720a60982cbaf3b9e317eccc3
Anseunghyeon <ashaaa1106@naver.com>
2024-08-01 16:24:25 +0900

    feat: add images

--- src/main/kotlin/bob13/presentation/controller/keypadController.kt
+++ src/main/kotlin/bob13/presentation/controller/keypadController.kt
@@ -1,10 +1,10 @@
 package bob13.presentation.controller
 
 import bob13.domain.service.KeypadService
-import org.springframework.stereotype.Controller
+
 import org.springframework.web.bind.annotation.*
 
-@Controller
+@RestController
 @RequestMapping("/keypad")
 class KeypadController(
     private val keypadService: KeypadService
